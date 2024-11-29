f94931af02ece9927809292e4708df7d9cb47c5c
Alyssa Resh <aresh@zipcoders-mbp.mynetworksettings.com>
2024-11-29 13:04:12 -0500

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,31 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer in Jan 2024
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        
-        int number = getInputNumber();
-        
-        int sum = sumOfNumbers(number);
-        
+        Main main = new Main();
+        //do I need to declare new object main? 
+        int num = main.getInputNumber();
+        int sum = main.sumOfNumbers(num);
+        int gaussSum = main.gaussianSumOfNumbers(num);
         System.out.println(sum);
-
+        System.out.println(gaussSum);
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
     int sumOfNumbers(int n) {
-        return 0;
+        int sum = 0;
+        int i = 0;
+        while (i <= n) {
+            sum = sum + i;
+            ++i;
+        }
+        return sum;
     }
-    
+
     
     int getInputNumber() { // gee, may thi sis useful in another lab?
         Scanner scan = new Scanner(System.in);
@@ -31,15 +38,17 @@ public class Main {
         // Closing Scanner after the use
         scan.close();
         return num;
+
     }
-    
-    
-    
+
+
+
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
-        return 0;
+        return (n * (n + 1)) / 2;
     }
-   
+
+
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
